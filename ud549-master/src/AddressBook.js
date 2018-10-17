4b343b2a0bf419b748545167c4678ef6d371df11
Ebere Uzodufa <ebereuzodufa@gmail.com>
2018-10-17 09:09:32 +0100

    Add files via upload
    feat: updating repo
    
    Edited codes and added a new feature. The new feature is for asynchronous testing.

--- ud549-master/src/AddressBook.js
+++ ud549-master/src/AddressBook.js
@@ -3,6 +3,18 @@ function AddressBook(){
 	// 	// let thisContact = this.contact
 	// }
 	this.contacts = [];
+	this.initialComplete = false;
+}
+
+AddressBook.prototype.getInitialContacts = function(cb){
+	var self = this;
+
+	setTimeout(function(){
+		self.initialComplete = true;
+		if (cb) {
+			return cb();
+		}
+	}, 3);
 }
 
 AddressBook.prototype.addContact = function(contact){
@@ -11,4 +23,8 @@ AddressBook.prototype.addContact = function(contact){
 
 AddressBook.prototype.getContact = function(index){
 	return this.contacts[index];
+}
+
+AddressBook.prototype.deleteContact = function(index){
+	return this.contacts.splice(index, 1);
 }
